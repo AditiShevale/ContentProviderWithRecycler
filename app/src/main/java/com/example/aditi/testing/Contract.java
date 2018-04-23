021376c0c9a10bd16bf7d280b5c3420c871b5957
aditi.shevale999@gmail.com <aditi.shevale999@gmail.com>
2018-04-23 10:55:12 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aditi/testing/Contract.java
@@ -0,0 +1,23 @@
+package com.example.aditi.testing;
+
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+public class Contract {
+
+    public static final String AUTHORITY
+            = "com.example.aditi.testing";
+
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + AUTHORITY);
+
+    public static final String PATH_TASKS = "details";
+
+    public static final class Entry implements BaseColumns{
+
+        public static final Uri CONTENT_URI =
+                BASE_CONTENT_URI.buildUpon().appendPath(PATH_TASKS).build();
+
+        public static final String TABLE_NAME = "details";
+        public static final String COLUMN_NAME = "name";
+    }
+}
